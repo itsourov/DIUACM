45f8da5a255e6ec51ddeedbd7b710c0dd4f94f93
Sourov Biswas <sourov2305101004@diu.edu.bd>
2024-09-15 12:54:01 +0600

    janina

--- app/Http/Controllers/TrackerController.php
+++ app/Http/Controllers/TrackerController.php
@@ -98,9 +98,10 @@
 							continue;
 						}
 						
-						$usersData[$user->id]['solve_score'] += ($event->weight * $usersData[$user->id][$event->id]['solve_count'] ?? 0);
+						
+						$usersData[$user->id]['solve_score'] += ($event->weight * ($usersData[$user->id][$event->id]['solve_count'] ?? 0));
 						if ($tracker->count_upsolve)
-							$usersData[$user->id]['upsolve_score'] += 0.25 * ($event->weight * $usersData[$user->id][$event->id]['upsolve_count'] ?? 0);
+							$usersData[$user->id]['upsolve_score'] += 0.25 * ($event->weight * ($usersData[$user->id][$event->id]['upsolve_count'] ?? 0));
 						
 					} catch (ConnectionException $e) {
 						
