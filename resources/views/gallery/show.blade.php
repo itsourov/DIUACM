e671d0f0dca6c05030bc4664db4ee7d893e74e45
Sourov Biswas <sourov2305101004@diu.edu.bd>
2024-09-18 20:47:54 +0600

    dont load full quality images temp

--- resources/views/gallery/show.blade.php
+++ resources/views/gallery/show.blade.php
@@ -14,7 +14,7 @@
 		    @foreach ($gallery->getMedia("gallery-images") as $image)
 			    <a
 					    class="group relative block overflow-hidden rounded-lg spotlight"
-					    href="{{$image->getUrl()}}">
+					    href="{{$image->getUrl('medium')}}">
 				    <img
 						    class="aspect-1.5  w-full rounded-lg bg-gray-100 object-cover dark:bg-neutral-800"
 						    src="{{$image->getUrl('medium')}}"
