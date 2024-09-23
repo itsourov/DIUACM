c3c980c716251fca40cbeb79d21e87e35ea6ecf0
Sourov Biswas <sourov2305101004@diu.edu.bd>
2024-09-23 23:02:46 +0600

    ..

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,7 +40,7 @@
 				->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
 				->widgets([
 					Widgets\AccountWidget::class,
-					Widgets\FilamentInfoWidget::class,
+//					Widgets\FilamentInfoWidget::class,
 				])
 				->middleware([
 					EncryptCookies::class,
