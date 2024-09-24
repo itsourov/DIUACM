eb0381b373c0b088af8ec453e451040b998c7172
Sourov Biswas <sourov2305101004@diu.edu.bd>
2024-09-24 17:50:53 +0600

    contact-page.blade.php

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,2 +1,2 @@
-<div {{ $attributes->merge(['class' => 'p-5 rounded-lg bg-white dark:bg-black shadow-md  hover:shadow-lg dark:shadow-md dark:hover:shadow-lg dark:shadow-gray-800 hover:dark:shadow-gray-800 ']) }}>
+<div {{ $attributes->merge(['class' => 'p-5 rounded-lg bg-white dark:bg-black border dark:border-neutral-700']) }}>
 	{{ $slot }}</div>
