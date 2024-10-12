5a329a8ecb39d647fdcfdd7dd3d81a85668fb76c
Sourov Biswas <itsourov12@gmail.com>
2024-10-12 15:13:53 +0600

    carousel

--- resources/views/gallery/show.blade.php
+++ resources/views/gallery/show.blade.php
@@ -4,7 +4,7 @@
 	    <div class="mb-4 flex items-center justify-between gap-8 sm:mb-8 md:mb-12">
 		    <div class="relative mx-auto">
 			    <h2 class="text-center text-3xl leading-8 font-extrabold tracking-tight sm:text-4xl">{{$gallery->title}}</h2>
-			    <p class="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">{{$gallery->description}} asd </div>
+			    <p class="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">{{$gallery->description}} </div>
 
 
 	    </div>
