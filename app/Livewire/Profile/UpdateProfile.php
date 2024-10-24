14434412824960da051b9b263942cb1c500231d0
Sourov Biswas <itsourov12@gmail.com>
2024-10-25 01:19:11 +0600

    allow underscore

--- app/Livewire/Profile/UpdateProfile.php
+++ app/Livewire/Profile/UpdateProfile.php
@@ -1,7 +1,7 @@
 <?php
-	
+
 	namespace App\Livewire\Profile;
-	
+
 	use App\Models\User;
 	use Filament\Forms;
 	use Filament\Forms\Components\Actions\Action;
@@ -13,33 +13,33 @@
 	use Filament\Notifications\Notification;
 	use Illuminate\Contracts\View\View;
 	use Livewire\Component;
-	
+
 	class UpdateProfile extends Component implements HasForms
 	{
 		use InteractsWithForms;
-		
+
 		public ?array $data = [];
-		
+
 		public User $record;
-		
+
 		public function mount(User $user): void
 		{
 			$this->record = $user;
 			// dd($user);
 			$this->form->fill($this->record->attributesToArray());
 		}
-		
+
 		public function form(Form $form): Form
 		{
-			
+
 			return $form
 				->schema([
-					
+
 					Section::make('Profile Information')
 						->columns(2)
 						->description('Update your account\'s profile information and email address.')
 						->schema([
-							
+
 							SpatieMediaLibraryFileUpload::make('profile Image')
 								->collection('profile-images')
 								->disk('profile-images')
@@ -61,15 +61,15 @@
 									'1:1',
 								])
 								->visibility('public'),
-							
+
 							Forms\Components\TextInput::make('name')
 								->required()
 								->maxLength(255),
-							
+
 							Forms\Components\TextInput::make('username')
 								->required()
 								->helperText('Cannot contain spaces or special characters.')
-								->rules(['required', 'regex:/^[a-zA-Z0-9-]+$/'])
+                                ->rules(['required', 'regex:/^[a-zA-Z0-9_-]+$/'])
 								->unique(ignoreRecord: true)
 								->prefixIcon('heroicon-o-at-symbol')
 								->suffixAction(
@@ -81,16 +81,16 @@
 //										})
 										->openUrlInNewTab()
 										->requiresConfirmation()
-								
+
 								)
 								->maxLength(255),
-							
+
 							Forms\Components\TextInput::make('email')
 								->email()
 								->disabled()
 								->required()
 								->maxLength(255),
-							
+
 							Forms\Components\TextInput::make('student_id')
 								->prefixIcon('heroicon-o-identification')
 								->required()
@@ -99,7 +99,7 @@
 								->required()
 								->prefixIcon('heroicon-o-phone')
 								->maxLength(255),
-							
+
 							Forms\Components\TextInput::make('codeforces_username')
 								->required()
 								->suffixAction(
@@ -111,7 +111,7 @@
 										})
 										->openUrlInNewTab()
 										->requiresConfirmation()
-								
+
 								)
 								->maxLength(255),
 							Forms\Components\TextInput::make('vjudge_username')
@@ -125,7 +125,7 @@
 										})
 										->openUrlInNewTab()
 										->requiresConfirmation()
-								
+
 								)
 								->maxLength(255),
 							Forms\Components\TextInput::make('atcoder_username')
@@ -139,28 +139,28 @@
 										})
 										->openUrlInNewTab()
 										->requiresConfirmation()
-								
+
 								)
 								->maxLength(255),
-							
+
 							Forms\Components\Textarea::make('bio')
 								->maxLength(255),
-						
-						
+
+
 						])
-				
-				
+
+
 				])
 				->statePath('data')
 				->model($this->record);
 		}
-		
+
 		public function save(): void
 		{
 			$data = $this->form->getState();
-			
+
 			$this->record->fill($data);
-			
+
 			if ($this->record->isDirty('email')) {
 				$this->record->email_verified_at = null;
 			}
@@ -170,7 +170,7 @@
 				->success()
 				->send();
 		}
-		
+
 		public function render(): View
 		{
 			return view('livewire.profile.update-profile');
