121feef9ad0fd877a2874027d52aa87812584245
Sourov Biswas <itsourov12@gmail.com>
2024-10-25 22:39:14 +0600

    ok

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,7 +1,7 @@
 <?php
-	
+
 	namespace App\Models;
-	
+
 
 	use App\Enums\AccessStatuses;
 	use App\Enums\VisibilityStatuses;
@@ -16,12 +16,12 @@
 	use Spatie\MediaLibrary\HasMedia;
 	use Spatie\MediaLibrary\InteractsWithMedia;
 	use Spatie\MediaLibrary\MediaCollections\Models\Media;
-	
+
 	class Post extends Model  implements HasMedia
 	{
 		use InteractsWithMedia;
 		use SoftDeletes, HasFactory;
-		
+
 		protected $fillable = [
 			'title',
 			'slug',
@@ -30,19 +30,19 @@
 			'status',
 			'user_id',
 		];
-		
+
 		protected function casts(): array
 		{
 			return [
 				'status' => VisibilityStatuses::class,
 			];
 		}
-		
+
 		public function categories()
 		{
 			return $this->belongsToMany(Category::class, );
 		}
-		
+
 		public function user(): BelongsTo
 		{
 			return $this->belongsTo(User::class);
@@ -51,27 +51,27 @@
 		{
 			return !$this->isStatusPublished();
 		}
-		
+
 		public function scopePublished(Builder $query)
 		{
-			return $query->where('status', VisibilityStatuses::PUBLISHED)->latest('created_at');
+			return $query->where('status', VisibilityStatuses::PUBLISHED)->latest('updated_at');
 		}
 		public function scopeScheduled(Builder $query)
 		{
 			return $query->where('status', VisibilityStatuses::SCHEDULED)->latest('scheduled_for');
 		}
-		
+
 		public function scopePending(Builder $query)
 		{
 			return $query->where('status', VisibilityStatuses::PENDING)->latest('created_at');
 		}
-		
-		
+
+
 		public function formattedPublishedDate()
 		{
 			return $this->created_at?->format('d M Y');
 		}
-		
+
 		public function isScheduled()
 		{
 			return $this->status === VisibilityStatuses::SCHEDULED;
@@ -87,7 +87,7 @@
 					->whereNotIn('posts.id', [$this->id]);
 			})->published()->take($take)->get();
 		}
-		
+
 		public static function getForm()
 		{
 			return PostForm::getComponents();
@@ -99,7 +99,7 @@
 				->fit(Fit::Contain, 400, 300)
 				->queued();
 		}
-		
+
 		public function comments(): MorphMany
 		{
 			return $this->morphMany(Comment::class, 'commentable');
