fb7bc82c36f945acf797d83427f660759fe48b48
Sourov Biswas <itsourov12@gmail.com>
2024-11-03 18:02:41 +0600

    ok

--- app/Livewire/RankList.php
+++ app/Livewire/RankList.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Enums\AccessStatuses;
 use App\Enums\UserType;
 use App\Models\SolveCount;
 use App\Models\Tracker;
@@ -31,38 +32,57 @@ class RankList extends Component
 
     public function render(): Factory|View|Application|\Illuminate\View\View
     {
-        $contests = $this->tracker->events;
+        $tracker = $this->tracker;
+
+        $contests = cache()->remember($tracker->id . '_contests', 60 * 60 * 2, function () use ($tracker) {
+            return $tracker->events;
+        });
         $eventIds = $contests->pluck('id')->toArray();
 
-// Convert the events collection to an array with event ID as key and weight as value for easy lookup
+        // Convert the events collection to an array with event ID as key and weight as value for easy lookup
         $eventWeights = $contests->pluck('weight', 'id')->toArray();
 
-        $users = User::select(['id', 'name'])
-            ->with(['solveCounts' => function ($query) use ($eventIds) {
-                $query->whereIn('event_id', $eventIds);
-            }, 'media'])
-            ->get()
-            ->map(function ($user) use ($eventWeights) {
-                $score = 0;
+        $users = cache()->remember('ranklist_' . $tracker->id, 60 * 60 * 2, function () use ($eventIds, $tracker, $eventWeights) {
+            return User::select(['id', 'name'])
+                ->with(['solveCounts' => function ($query) use ($eventIds) {
+                    $query->whereIn('event_id', $eventIds);
+                }, 'media'])
+                ->when($this->tracker->organized_for == AccessStatuses::SELECTED_PERSONS, function ($query) use ($tracker) {
+                    return $query->whereIn('id', function ($query) use ($tracker) {
+                        $query->select('user_id')
+                            ->from('group_user')
+                            ->join('groups', 'group_user.group_id', '=', 'groups.id')
+                            ->whereIn('groups.id', function ($query) use ($tracker) {
+                                $query->select('group_id')
+                                    ->from('group_tracker')
+                                    ->where('tracker_id', $tracker->id);
+                            });
+                    });
+                })
+                ->get()
+                ->map(function ($user) use ($eventWeights) {
+                    $score = 0;
 
-                // Key solveCounts by event_id for easier access
-                $solveCounts = $user->solveCounts->keyBy('event_id');
+                    // Key solveCounts by event_id for easier access
+                    $solveCounts = $user->solveCounts->keyBy('event_id');
 
-                foreach ($solveCounts as $eventId => $solveCount) {
-                    $weight = $eventWeights[$eventId] ?? 1; // Default to weight 1 if not specified
+                    foreach ($solveCounts as $eventId => $solveCount) {
+                        $weight = $eventWeights[$eventId] ?? 1; // Default to weight 1 if not specified
 
-                    // Calculate weighted score
-                    $score += ($solveCount->solve_count * $weight) + ($solveCount->upsolve_count * $weight / 2);
-                }
+                        // Calculate weighted score
+                        $score += ($solveCount->solve_count * $weight) + ($solveCount->upsolve_count * $weight / 2);
+                    }
 
-                // Assign the calculated score to the user model's `score` attribute
-                $user->score = $score;
-                $user->solveCounts = $solveCounts; // Reassign the keyed solveCounts for direct access by event_id
+                    // Assign the calculated score to the user model's `score` attribute
+                    $user->score = $score;
+                    $user->solveCounts = $solveCounts; // Reassign the keyed solveCounts for direct access by event_id
 
-                return $user;
-            })
-            ->sortByDesc('score') // Sort by score after mapping
-            ->values();
+                    return $user;
+                })
+                ->sortByDesc('score') // Sort by score after mapping
+                ->values();
+        });
+
 
         return view('livewire.rank-list', compact('users', 'contests'));
     }
