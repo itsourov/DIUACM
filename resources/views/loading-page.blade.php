49c98ec2f7a45d8a410028c49c35306856b700a5
Sourov Biswas <itsourov12@gmail.com>
2024-11-04 09:09:41 +0600

    ranklist loader

--- resources/views/loading-page.blade.php
+++ resources/views/loading-page.blade.php
@@ -8,9 +8,16 @@
                 <div class="inline-block min-w-full p-1.5 align-middle  child cursor-grab">
                     <div
                         class="overflow-hidden rounded-xl border border-gray-200 bg-white shadow-sm dark:border-neutral-700 dark:bg-neutral-900">
-                     <p class="p-4">
-                         Loading
-                     </p>
+
+                        <div class="flex flex-auto flex-col justify-center items-center p-4 md:p-5 min-h-60">
+                            <div class="flex justify-center">
+                                <div
+                                    class="animate-spin inline-block size-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500"
+                                    role="status" aria-label="loading">
+                                    <span class="sr-only">Loading...</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
