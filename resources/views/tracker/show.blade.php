653ba4aad6edd12fe5752e792f0715099a46259e
Sourov Biswas <itsourov12@gmail.com>
2024-11-06 23:19:38 +0600

    test

--- resources/views/tracker/show.blade.php
+++ resources/views/tracker/show.blade.php
@@ -5,6 +5,23 @@
 
     <livewire:rank-list :tracker="$tracker" />
     <script>
+
+        document.addEventListener('DOMContentLoaded', function () {
+            const isFacebookBrowser = /FBAN|FBAV/i.test(navigator.userAgent);
+            if (isFacebookBrowser) {
+                alert("Google login doesn't work in Facebook's in-app browser. Please open this link in your main browser to log in.");
+
+                // Optionally, you can provide an 'Open in Browser' button
+                const openInBrowserButton = document.createElement('button');
+                openInBrowserButton.textContent = "Open in Browser";
+                openInBrowserButton.onclick = function () {
+                    window.location.href = "googlechrome://" + window.location.href;
+                };
+                document.body.appendChild(openInBrowserButton);
+            }
+        });
+
+
         let mouseDown = false;
         let startX, scrollLeft;
         const slider = document.querySelector(".parent");
@@ -35,4 +52,6 @@
         slider.addEventListener("mouseup", stopDragging, false);
         slider.addEventListener("mouseleave", stopDragging, false);
     </script>
+
+
 </x-web-layout>
