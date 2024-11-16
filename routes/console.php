d170115056989db7c26433cc8139cc6538ae9e99
Sourov Biswas <itsourov12@gmail.com>
2024-11-16 23:13:09 +0600

    recalculate score console

--- routes/console.php
+++ routes/console.php
@@ -7,4 +7,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('bot:update-trackers')->everyTwoHours()->withoutOverlapping();
+Schedule::command('bot:update-trackers')->everyFourHours()->withoutOverlapping();
