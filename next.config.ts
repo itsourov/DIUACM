0bf5e118d033927cbba5138e03c4b414157436a2
Sourov Biswas <sourov2305101004@diu.edu.bd>
2025-04-07 14:01:07 +0600

    refactor: Add images configuration to next.config.ts for domain whitelisting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "nextacm.sgp1.cdn.digitaloceanspaces.com",
+      "cdn.diuacm.com",
+    ],
+  },
 };
 
 export default nextConfig;
