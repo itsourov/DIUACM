5d042d6b99380c3299adcf0de0e96a06c1a3a854
Sourov Biswas <sourovcodes@gmail.com>
2025-07-25 17:20:59 +0600

    feat: add custom 404 Not Found page with navigation options

--- app/(public)/not-found.tsx
+++ app/(public)/not-found.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Button } from "../components/ui/button";
+import { Button } from "@/components/ui/button";
 import { FileQuestion } from "lucide-react";
 import React from "react";
 
